16aeb248a1c15d08fef3b78ba41fbdcedcef1160
mayeskhatri243 <mayeskhatri243@gmail.com>
2022-09-30 16:28:16 +0530

    assignment completed

--- client/src/components/Blogs/index.js
+++ client/src/components/Blogs/index.js
@@ -18,49 +18,49 @@ const Blog = () => {
   }, []);
 
   return (
-    <section class="pt-4 pb-36 bg-white overflow-hidden">
-      <div class="container px-4 mx-auto">
-        <p class="mb-4 text-3xl text-center font-bold font-heading font-heading tracking-px-n leading-tight">
+    <section className="pt-4 pb-36 bg-white overflow-hidden">
+      <div className="container px-4 mx-auto">
+        <p className="mb-4 text-3xl text-center font-bold font-heading font-heading tracking-px-n leading-tight">
           Latest from Blog
         </p>
 
-        <div class="flex flex-wrap -m-8">
+        <div className="flex flex-wrap -m-8">
           {/*  */}
           {blogs?.map((blog, index) => (
-            <div class="w-full md:w-1/2 p-8" key={index}>
-              <div class="flex flex-wrap lg:items-center -m-4">
-                <div class="w-auto p-4">
-                  <div class="overflow-hidden rounded-xl">
+            <div className="w-full md:w-1/2 p-8" key={index}>
+              <div className="flex flex-wrap lg:items-center -m-4">
+                <div className="w-auto p-4">
+                  <div className="overflow-hidden rounded-xl">
                     <img
-                      class="transform hover:scale-105 transition ease-in-out duration-1000"
+                      className="transform hover:scale-105 transition ease-in-out duration-1000"
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv85bGgBVHECXS9UL8hhyjzIInP23uFTRzFyo4zeTNYA&s"
                       alt={blog.title}
                     />
                   </div>
                 </div>
-                <div class="flex-1 p-4">
-                  <div class="md:max-w-xs">
-                    <div class="flex flex-col justify-between h-full">
-                      <div class="mb-6">
-                        <p class="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
+                <div className="flex-1 p-4">
+                  <div className="md:max-w-xs">
+                    <div className="flex flex-col justify-between h-full">
+                      <div className="mb-6">
+                        <p className="mb-1.5 text-sm text-gray-500 font-medium uppercase tracking-px">
                           {blog?.title}
                         </p>
                         <a
-                          class="inline-block hover:text-gray-800 hover:underline"
+                          className="inline-block hover:text-gray-800 hover:underline"
                           href="#"
                         >
-                          <h3 class="text-xl font-semibold leading-normal">
+                          <h3 className="text-xl font-semibold leading-normal">
                             {blog?.shortDesc}
                           </h3>
                         </a>
                       </div>
-                      <div class="flex flex-wrap items-center -m-1">
-                        <div class="w-auto p-1">
-                          <p class="text-sm font-semibold leading-relaxed">
+                      <div className="flex flex-wrap items-center -m-1">
+                        <div className="w-auto p-1">
+                          <p className="text-sm font-semibold leading-relaxed">
                             - {blog?.createdBy}
                           </p>
                         </div>
-                        <p class="text-sm font-semibold leading-relaxed">
+                        <p className="text-sm font-semibold leading-relaxed">
                           - {moment(blog?.createdAt).format("MMM Do YY")}
                         </p>
                       </div>
